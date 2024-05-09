8a9597aafcba946dc90ff01a2cd25996ebebb67b
cristinewunderlich <cris.negresco@gmail.com>
2024-05-09 15:55:03 -0300

    criado projeto e suite de pesquisa

---
+++ cypress/e2e/search.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="Cypress"/>
+
+describe('Blog - pesquisar', () => {
+  it('pesquisa com resultados encontrados', () => {
+    cy.visit('/')
+    cy.get('div[data-section=section-header-search]').should('be.visible').click()
+    cy.get('input[type=search]').should('be.visible').type('Agibank{enter}')
+    cy.get('.page-title').should('be.visible')
+    // Resultados encontrados para: agibank
+    cy.get('.page-title').children().should('have.text', 'Agibank')
+  })
+
+  it('pequisar sem resultados encontrados', () =>{
+    cy.visit('/')
+    cy.get('div[data-section=section-header-search]').should('be.visible').click()
+    cy.get('input[type=search]').should('be.visible').type('****{enter}')
+    cy.get('.page-title').should('be.visible')
+    cy.get('.page-title').children().should('have.text', '****')
+    cy.get('.page-content').children('p').should('have.text', 'Lamentamos, mas nada foi encontrado para sua pesquisa, tente novamente com outras palavras.')
+  })
+
+  it('SQL injection', () =>{
+    cy.visit('/')
+    cy.get('div[data-section=section-header-search]').should('be.visible').click()
+    cy.get('input[type=search]').should('be.visible').type(`' OR 1=1 --{enter}`)
+    cy.get('.page-title').should('be.visible')
+    cy.get('.page-title').children().should('have.text', `' OR 1=1 --`)
+    cy.get('.page-content').children('p').should('have.text', 'Lamentamos, mas nada foi encontrado para sua pesquisa, tente novamente com outras palavras.')
+  })
+})
