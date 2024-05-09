768907ae5cc815b0688838478934e0e22268f95f
cristinewunderlich <cris.negresco@gmail.com>
2024-05-09 17:17:08 -0300

    adicionar comando personalisado

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -7,24 +7,18 @@ describe('Blog - pesquisar', () => {
   })
 
   it('pesquisa com resultados encontrados', () => {
-    cy.get('div[data-section=section-header-search]').should('be.visible').click()
-    cy.get('input[type=search]').should('be.visible').type('Agibank{enter}')
-    cy.get('.page-title').should('be.visible')
+    cy.pesquisar('Agibank')
     cy.get('.page-title').children().should('have.text', 'Agibank')
   })
 
   it('pequisar sem resultados encontrados', () => {
-    cy.get('div[data-section=section-header-search]').should('be.visible').click()
-    cy.get('input[type=search]').should('be.visible').type('****{enter}')
-    cy.get('.page-title').should('be.visible')
+    cy.pesquisar('****')
     cy.get('.page-title').children().should('have.text', '****')
     cy.get('.page-content').children('p').should('have.text', 'Lamentamos, mas nada foi encontrado para sua pesquisa, tente novamente com outras palavras.')
   })
 
   it('SQL injection', () => {
-    cy.get('div[data-section=section-header-search]').should('be.visible').click()
-    cy.get('input[type=search]').should('be.visible').type(`' OR 1=1 --{enter}`)
-    cy.get('.page-title').should('be.visible')
+    cy.pesquisar(`' OR 1=1 --`)
     cy.get('.page-title').children().should('have.text', `' OR 1=1 --`)
     cy.get('.page-content').children('p').should('have.text', 'Lamentamos, mas nada foi encontrado para sua pesquisa, tente novamente com outras palavras.')
   })
